84adcacf706ef8a77dbc4be853de8154be63b673
“Carolina <carolina.rioslc@gmail.com>
2025-05-18 22:55:42 -0600

    Primer commit, inicio de componente

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    //fragment sin div
+    <>
+      <div>
+        <label>Selecciona un Pais: </label>
+          <select name=''>
+          </select>
+      </div>
+    </>
+  )
+}
+
+export default App
